5a2fdd3e8f88cd34c744dd7d93b700e6933f399c
zby <zboyu0104@aliyun.com>
2021-12-22 16:15:03 +0800

    ExampleCountSource

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/source/SourceFunction.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/source/SourceFunction.java
@@ -46,14 +46,16 @@ import java.io.Serializable;
  * <pre>{@code
  *  public class ExampleCountSource implements SourceFunction<Long>, CheckpointedFunction {
  *      private long count = 0L;
+ *      // 使用一个volatile类型变量控制run方法内循环的运行
  *      private volatile boolean isRunning = true;
- *
+ *      // 保存数据源状态的变量
  *      private transient ListState<Long> checkpointedCount;
  *
  *      public void run(SourceContext<T> ctx) {
  *          while (isRunning && count < 1000) {
  *              // this synchronized block ensures that state checkpointing,
  *              // internal state updates and emission of elements are an atomic operation
+ *              // 此处必须要加锁，防止在checkpoint过程中，仍然发送数据
  *              synchronized (ctx.getCheckpointLock()) {
  *                  ctx.collect(count);
  *                  count++;
@@ -62,14 +64,16 @@ import java.io.Serializable;
  *      }
  *
  *      public void cancel() {
+ *      	// 设置isRunning为false，终止run方法内循环的运行
  *          isRunning = false;
  *      }
  *
  *      public void initializeState(FunctionInitializationContext context) {
+ *     		// 获取存储状态
  *          this.checkpointedCount = context
  *              .getOperatorStateStore()
  *              .getListState(new ListStateDescriptor<>("count", Long.class));
- *
+ *			// 如果数据源是从失败中恢复，则读取count的值，恢复数据源count状态
  *          if (context.isRestored()) {
  *              for (Long count : this.checkpointedCount.get()) {
  *                  this.count = count;
@@ -78,6 +82,7 @@ import java.io.Serializable;
  *      }
  *
  *      public void snapshotState(FunctionSnapshotContext context) {
+ *      	// 保存数据到状态变量
  *          this.checkpointedCount.clear();
  *          this.checkpointedCount.add(count);
  *      }
